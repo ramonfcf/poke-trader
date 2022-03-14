b8c1f018364c31cd3d6dbc652e7cdf522b4cc2de
ramonfcf <ramonfcf@gmail.com>
2022-03-14 12:55:45 -0300

    organiznado melhor o script, adicionando funções

---
+++ script/classe.js
@@ -0,0 +1,70 @@
+class Pokemon {
+    id
+    nome
+    baseExp
+    constructor(id, nome, baseExp) {
+        this.id = id,
+        this.nome = nome,
+        this. baseExp = baseExp;
+    }
+
+}
+
+class Registro{
+    time1
+    baseExpT1
+    time2
+    baseExpT2
+
+    constructor(time1, baseExpSomaT1, time2, baseExpSomaT2){
+        this.time1 = time1;
+        this.baseExpT1 = baseExpSomaT1;
+        this.time2 = time2;
+        this.baseExpT2 = baseExpSomaT2;
+    }
+
+    imprimir(){
+
+    }
+}
+
+class BancoDados {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if (id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    verificarId(){
+        let proximoId = localStorage.getItem('id');
+        return Number(proximoId) + 1;
+    }
+
+
+    gravar(pokemons){
+        let id = this.verificarId();
+
+        localStorage.setItem(id, JSON.stringify(pokemons))
+
+        localStorage.setItem('id', id);
+    }
+
+    recuperarPokemons(){
+        let pokemonsTime = [];
+
+        let id = localStorage.getItem('id');
+
+        for(let i = 1; i<= id; i++){
+            let time1 = JSON.parse(localStorage.getItem(i))
+
+            if (time1 == null) {
+                continue
+            }
+            pokemonsTime.push(time1);
+        }
+        console.log(pokemonsTime)
+    }
+}
---
+++ script/script.js
@@ -0,0 +1,186 @@
+let arrayPokemons = []; //array de pokemons buscados na api de acordo com o id inserido no input do jogador
+let time1PokemonsId = [];
+let time2PokemonsId = [];
+
+let time1Pokemons = [];
+let time2Pokemons = [];
+
+let registro = new Registro()
+let bancoDados = new BancoDados()
+
+let botaoTime1 = document.querySelector('#botao-time-1');
+let botaoTime2 = document.querySelector('#botao-time-2');
+let botaoCalcular = document.querySelector('#botao-calcular')
+
+//Click botão time 1
+botaoTime1.addEventListener("click", function(){
+    let inputIdPokemonT1 = document.querySelector('#t1-pokemon');
+
+    if (inputIdPokemonT1.value == 0){
+        window.alert('Insira um Pokemon no time 1')
+    } else if (inputIdPokemonT1.value <= 0 || inputIdPokemonT1.value > 150){
+        window.alert('Insira o número de um Pokemon entre 1 e 150')
+    } else if (time1PokemonsId.length == 5) {
+        window.alert('O time 1 está completo')
+    }else {
+        adicionarPokemonTime1();
+    }
+})
+
+//Click botão time 2
+botaoTime2.addEventListener("click", function(){
+    let inputIdPokemonT2 = document.querySelector('#t2-pokemon');
+
+    if (inputIdPokemonT2.value == 0){
+        window.alert('Insira um Pokemon no Time 2')
+    } else if (inputIdPokemonT2.value <= 0 || inputIdPokemonT2.value > 150){
+        window.alert('Insira o número de um Pokemon entre 1 e 150')
+    } else if (time2PokemonsId.length == 5) {
+        window.alert('O time 2 está completo')
+    } else {
+        adicionarPokemonTime2();
+    }
+})
+
+//Click botão calcular
+botaoCalcular.addEventListener("click", function(){
+    montaTimePokemon(time1PokemonsId, time1Pokemons);
+
+    let baseExpT1 = somaBaseExp(time1Pokemons);    
+    //let baseExpT2 = somaBaseExp(time2Pokemons);
+
+    console.log(baseExpT1)
+   console.log(time1Pokemons)
+
+})
+
+// Função que recebe dado do input do time um, busca o nome e id na api e insere no html pra visualização do usuário, adiciona a id no time1PokemonsId
+function adicionarPokemonTime1(){
+    let inputIdPokemonT1 = document.querySelector('#t1-pokemon');
+    var pokemonId = inputIdPokemonT1.value;
+    let tbody = document.querySelector('#tbody-time-1');
+
+    let nome = recebeDadosPokemon(inputIdPokemonT1.value).then(data => {
+        let linha = tbody.insertRow('tr')
+        linha.insertCell(0).innerHTML = `${data.nome} - ${data.id}`;
+    })
+
+    time1PokemonsId.push(pokemonId);
+    inputIdPokemonT1.focus();
+}
+
+// Função que recebe dado do input do time um, busca o nome e id na api e insere no html pra visualização do usuário, adiciona a id no time1PokemonsId
+function adicionarPokemonTime2(){
+    let inputIdPokemonT2 = document.querySelector('#t2-pokemon');
+    var pokemonId = inputIdPokemonT2.value;
+    let tbody = document.querySelector('#tbody-time-2');
+
+    let nome = recebeDadosPokemon(inputIdPokemonT2.value).then(data => {
+        let linha = tbody.insertRow('tr')
+        linha.insertCell(0).innerHTML = data.nome;
+    })
+
+    time2PokemonsId.push(pokemonId);
+    inputIdPokemonT2.focus();
+}
+
+
+//função com foreach para montar time pokemon
+function montaTimePokemon(time, variavel){
+        time.forEach(element => {
+        recebeDadosPokemon(element).then(data => variavel.push(data));
+    });
+}
+
+//função que busca dados do pokemon na api
+function recebeDadosPokemon(id){
+
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(res => res.json())
+    .then(data => {
+
+        let pokemon = new Pokemon (data.id, data.name, data.base_experience);
+            return pokemon;
+    })
+}
+
+
+//função que faz o cálculo de troca justa, sempre pegando o time de maior valor. 
+function calculaTroca(t1, t2){
+    if (t1 < t2) {
+        let diferenca = (t2 * 0.9).toFixed(2);
+        if(t1 > diferenca){
+            console.log('troca Justa')
+        } else {
+            console.log('troca injusta')
+        }
+    } else {
+        let diferenca = (t1 * 0.9).toFixed(2);
+        if(t2 > diferenca){
+            console.log('troca Justa')
+        } else {
+            console.log('troca injusta')
+        }
+    }
+}
+
+// função que soma todas as base experience dos pokemons de um time.
+function somaBaseExp(time){
+    let resultadoSoma = 0;
+    console.log(time[0]);
+
+    for (let i = 0; i < time.length; i++){
+        let base_experience = time[i].baseExp;
+        console.log(base_experience);
+        resultadoSoma = resultadoSoma + base_experience;
+    }
+
+    return resultadoSoma;
+}
+
+//console.log(arrayPokemons)
+
+
+
+
+
+
+// SEPARANDO
+/*let pokemons = [
+    {nome:'bulbasaur', base_experience: 64},
+    {nome:'ivysaur', base_experience: 142},
+    {nome:'venusaur', base_experience: 263},
+    {nome:'charmander', base_experience: 62},
+    {nome:'charmeleon', base_experience: 142},
+    {nome:'charizard', base_experience: 267},
+    {nome:'squirtle', base_experience: 63}
+]
+
+
+let team1 = [pokemons[2], pokemons[4], pokemons[5]];
+let team2 = [pokemons[0], pokemons[5], pokemons[2]];
+
+
+let valorBaseExpT1 = somaBaseExp(team1);  // 473
+let valorBaseExpT2 = somaBaseExp(team2);  // 672
+
+console.log(valorBaseExpT1)
+console.log(valorBaseExpT2)
+
+*/
+
+
+//calculaTroca(valorBaseExpT1, valorBaseExpT2)
+
+
+
+//criar uma verificação para no máximo 6 pokemons para cada time
+//criar a verifcaçãod de não achar pokemon na api
+
+
+/*registro.time1[i].nome
+
+function carregaListaComparacao(){
+    bancoDados.recuperarPokemons()
+}
+*/
